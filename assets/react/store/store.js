09499e72a2fc913e71fa0aaa0f9acd85f3232b02
Lucas-allard <allard.lucas.97@gmail.com>
2022-12-18 10:15:27 +0100

    chat : ajout d'un searchReducer

--- assets/react/store/store.js
+++ assets/react/store/store.js
@@ -1,11 +1,13 @@
 import {configureStore} from '@reduxjs/toolkit'
 import userReducer from "../features/user/userSlice";
 import channelReducer from "../features/channel/channelSlice";
+import searchReducer from "../features/search/searchSlice";
 
 
 export const store = configureStore({
     reducer: {
         user: userReducer,
         channel: channelReducer,
+        search: searchReducer,
     }
 })
