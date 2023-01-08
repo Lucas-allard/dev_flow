061f24ce270b83c0d09fb7330c72060f0037c00a
Lucas-allard <allard.lucas.97@gmail.com>
2023-01-08 17:04:47 +0100

    Datafixtures: UserFixtures

---
+++ src/DataFixtures/UserFixtures.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\User;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+use Faker;
+
+class UserFixtures extends Fixture
+{
+    public function load(ObjectManager $manager): void
+    {
+        $faker = Faker\Factory::create('fr_FR');
+
+        for ($u = 0; $u < 40; $u++) {
+            $user = new User();
+            $user->setEmail($faker->email);
+            $user->setPassword($faker->password);
+            $user->setRoles(['ROLE_USER']);
+            $user->setFullName($faker->name);
+            $user->setCreatedAt($faker->dateTimeBetween('-6 months'));
+            $user->setGoogleId($faker->uuid);
+
+            // create reference for later use
+            $this->addReference('user_' . $u, $user);
+        }
+    }
+}
