378db6d2882096041606f1eee3967dd84f247f94
Lucas-allard <allard.lucas.97@gmail.com>
2023-01-10 16:28:08 +0100

    Labo: refacto + add index_by_level

--- src/Controller/LaboController.php
+++ src/Controller/LaboController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use App\Repository\CategoryRepository;
 use App\Repository\CourseRepository;
+use App\Repository\LevelRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -11,39 +12,65 @@ use Symfony\Component\Routing\Annotation\Route;
 #[Route('/labo', name: 'labo_')]
 class LaboController extends AbstractController
 {
+
+    public function __construct(
+        private CourseRepository   $courseRepository,
+        private CategoryRepository $categoryRepository,
+        private LevelRepository    $levelRepository,
+    )
+    {
+    }
+
     /**
-     * @param CategoryRepository $categoryRepository
-     * @param CourseRepository $courseRepository
      * @return Response
      */
     #[Route('/', name: 'index')]
-    public function index(CategoryRepository $categoryRepository, CourseRepository $courseRepository): Response
+    public function index(): Response
     {
-        $categories = $categoryRepository->findAll();
-        $courses = $courseRepository->findCourses();
-
+        $categories = $this->categoryRepository->findAll();
+        $levels = $this->levelRepository->findAll();
+        $courses = $this->courseRepository->findCourses();
 
 
         return $this->render('labo/labo.html.twig', [
             "categories" => $categories,
+            "levels" => $levels,
             "courses" => $courses,
         ]);
     }
 
     /**
-     * @param CategoryRepository $categoryRepository
-     * @param CourseRepository $courseRepository
-     * @param $category
+     * @param string $category
      * @return Response
      */
-    #[Route('/{category}', name: 'category')]
-    public function category(CategoryRepository $categoryRepository, CourseRepository $courseRepository, $category): Response
+    #[Route('/catégories/{category}', name: 'by_category')]
+    public function category(string $category): Response
     {
-        $categories = $categoryRepository->findAll();
-        $courses = $courseRepository->findByCategory($category);
+        $categories = $this->categoryRepository->findAll();
+        $levels = $this->levelRepository->findAll();
+        $courses = $this->courseRepository->findByCategory($category);
 
         return $this->render('labo/labo.html.twig', [
             "categories" => $categories,
+            "levels" => $levels,
+            "courses" => $courses,
+        ]);
+    }
+
+    /**
+     * @param string $level
+     * @return Response
+     */
+    #[Route('/levels/{level}', name: 'by_level')]
+    public function level(string $level): Response
+    {
+        $categories = $this->categoryRepository->findAll();
+        $levels = $this->levelRepository->findAll();
+        $courses = $this->courseRepository->findByLevel($level);
+
+        return $this->render('labo/labo.html.twig', [
+            "categories" => $categories,
+            "levels" => $levels,
             "courses" => $courses,
         ]);
     }
