50eea5c5d63fe19ef85ded20225302c5ee038717
Lucas-allard <allard.lucas.97@gmail.com>
2023-01-11 10:21:17 +0100

    Notification: add notif to base.html.twig with react controller

--- assets/react/controllers/Notification.jsx
+++ assets/react/controllers/Notification.jsx
@@ -1,31 +1,27 @@
 import React, {useEffect} from 'react';
 import {ToastContainer, toast} from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 function Notification({type, message}) {
 
-    const notify = () => toast(message[0]);
+    const notify = () => toast(message[0], {
+        position: "top-right",
+        autoClose: 5000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+        theme: "dark",
+        type: type
+    });
 
     useEffect(() => {
         notify();
     }, [message]);
 
     return (
-        <div className={`alert alert-${type}`}>
-            <ToastContainer
-                position="top-right"
-                autoClose={5000}
-                hideProgressBar={false}
-                newestOnTop={false}
-                closeOnClick
-                rtl={false}
-                pauseOnFocusLoss
-                draggable
-                pauseOnHover
-                theme="light"
-            />
-            {/* Same as */}
-            <ToastContainer/>
-        </div>
+        <ToastContainer/>
     );
 }
 
