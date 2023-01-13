ff11e40442a6a7259e884fd796e00fa0c296adfd
Lucas-allard <allard.lucas.97@gmail.com>
2023-01-13 15:51:06 +0100

    Services: add a search form service

--- src/Services/SearchFormHandler.php
+++ src/Services/SearchFormHandler.php
@@ -2,7 +2,53 @@
 
 namespace App\Services;
 
+use App\Data\FilterData;
+use App\Repository\FilterableRepositoryInterface;
+use Symfony\Component\Form\FormFactoryInterface;
+use Symfony\Component\Form\FormInterface;
+use Symfony\Component\HttpFoundation\Request;
+
 class SearchFormHandler
 {
+    private $searchForm;
 
+    /**
+     * @param FormFactoryInterface $formFactory
+     * @param FilterableRepositoryInterface $repository
+     * @param string $formType
+     * @param FilterData $filterData
+     */
+    public function __construct(
+        private FormFactoryInterface          $formFactory,
+        private FilterableRepositoryInterface $repository,
+        string                                $formType,
+        private FilterData                            $filterData,
+    )
+    {
+        $this->searchForm = $this->formFactory->create($formType, $this->filterData);
+    }
+
+    /**
+     * @param Request $request
+     * @return array|null
+     */
+    public function handleSearchForm(Request $request): ?array
+    {
+        $this->searchForm->handleRequest($request);
+
+        if ($this->searchForm->isSubmitted() && $this->searchForm->isValid()) {
+
+            return $this->repository->findBySearch($this->filterData);
+        }
+
+        return null;
+    }
+
+    /**
+     * @return FormInterface
+     */
+    public function getSearchForm(): FormInterface
+    {
+        return $this->searchForm;
+    }
 }
