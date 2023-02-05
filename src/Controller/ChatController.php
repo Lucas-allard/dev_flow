009f72b12b1b0f5c0353aeaa565def2ef87761a8
Lucas-allard <allard.lucas.97@gmail.com>
2023-02-05 12:15:34 +0100

    update ChatController

--- src/Controller/ChatController.php
+++ src/Controller/ChatController.php
@@ -26,15 +26,12 @@ class ChatController extends AbstractController
 {
 
     /**
-     * @param CsrfTokenManagerInterface $csrfTokenManager
      * @param UserRepository $userRepository
      * @param FirestoreService $firestoreService
      */
     public function __construct(
-        private CsrfTokenManagerInterface $csrfTokenManager,
-        private UserRepository            $userRepository,
-        private FirestoreService          $firestoreService,
-        private UserNormalizer            $normalizer,
+        private UserRepository   $userRepository,
+        private FirestoreService $firestoreService,
     )
     {
     }
@@ -47,70 +44,45 @@ class ChatController extends AbstractController
     #[IsGranted('ROLE_USER')]
     public function index(): Response
     {
-        $csrfToken = $this->csrfTokenManager->getToken("send");
-
-        $user = $this->normalizer->normalize($this->getUser());
-
-        $user['csrfToken'] = $csrfToken->getValue();
-
-        $users = $this->normalizer->normalize($this->userRepository->findAll());
+        $user['id'] = $this->getUser()->getId();
+        $user['email'] = $this->getUser()->getEmail();
 
         return $this->render('chat/index.html.twig', [
-            'userData' => $user,
-            'users' => $users
+            'user' => $user,
         ]);
     }
 
     /**
      * @param Request $request
-     * @param ChatMessageRepository $chatMessageRepository
-     * @param EntityManagerInterface $entityManager
      * @return Response
      */
     #[Route('/send', name: 'send')]
     #[IsGranted('ROLE_USER')]
     public function send(
         Request                $request,
-        ChatMessageRepository  $chatMessageRepository,
-        EntityManagerInterface $entityManager
     ): Response
     {
         $messageData = json_decode($request->getContent(), true);
 
-        $messageData['message'] = strip_tags($messageData['message'], '<br>,<img>');
+        $this->cleanInput($messageData["message"]);
 
-        $messageData['message'] = preg_replace('#<img(.*?)src="(.*?)"(.*?)>#', '<img src="$2" alt="gif">', $messageData['message']);
+//            $user
+//                ->setPoints($user->getPoints() + 2)
+//                ->setChatMessageCount($user->getChatMessageCount() + 1);
+//
+//            $this->userRepository->save($user);
 
-        $messageData['message'] = htmlentities($messageData['message']);
-
-        if (!$this->csrfTokenManager->isTokenValid(new CsrfToken('chat', $messageData["user"]["csrfToken"]))) {
-
-            $user = $this->userRepository->find(["id" => $messageData["id"]]);
-
-            $chatMessage = new ChatMessage();
-            $chatMessage->setContent($messageData["message"])
-                ->setUser($user)
-                ->setTimestamp(new DateTime());
-
-            $user
-                ->setPoints($user->getPoints() + 2)
-                ->setChatMessageCount($user->getChatMessageCount() + 1);
-
-            $chatMessageRepository->save($chatMessage);
-            $this->userRepository->save($user);
-            $this->firestoreService->addDocument(
-                $messageData["collection"],
-                [
-                    "message" => $messageData["message"],
-                    "fullName" => $messageData["user"]["fullName"],
-                    "profilPicture" => $messageData["user"]["profilPicture"],
-                    "timestamp" => new FirestoreTimestamp(),
-                ]);
+        $this->firestoreService->addDocument(
+            $messageData["collection"],
+            [
+                "message" => $messageData["message"],
+                "fullName" => $messageData["user"]["fullName"],
+                "profilPicture" => $messageData["user"]["profilPicture"],
+                "timestamp" => new FirestoreTimestamp(),
+            ]);
 
 
-            $entityManager->flush();
-
-
+        if ($this->firestoreService->isSuccess()) {
             return new Response('', Response::HTTP_CREATED);
         } else {
             return new Response('', Response::HTTP_INTERNAL_SERVER_ERROR);
@@ -121,7 +93,8 @@ class ChatController extends AbstractController
      * @param Request $request
      * @return Response
      */
-    #[Route('/send/private', name: 'send_private')]
+    #[
+        Route('/send/private', name: 'send_private')]
     #[IsGranted('ROLE_USER')]
     public function sendPrivateMessage(
         Request $request,
@@ -129,27 +102,31 @@ class ChatController extends AbstractController
     {
         $messageData = json_decode($request->getContent(), true);
 
-        $messageData['message'] = strip_tags($messageData['message'], '<br>,<img>');
+        $messageData = $this->cleanInput($messageData["message"]);
 
-        $messageData['message'] = preg_replace('#<img(.*?)src="(.*?)"(.*?)>#', '<img src="$2" alt="gif">', $messageData['message']);
+        $this->firestoreService->addDocument(
+            $messageData["collection"],
+            [
+                "from" => $messageData["from"],
+                "to" => $messageData["to"],
+                "participants" => $messageData["participants"],
+                "message" => $messageData["message"],
+                "timestamp" => new FirestoreTimestamp(),
+            ]);
 
-        $messageData['message'] = htmlentities($messageData['message']);
-
-        if (!$this->csrfTokenManager->isTokenValid(new CsrfToken('chat', $messageData["user"]["csrfToken"]))) {
-
-            $this->firestoreService->addDocument(
-                $messageData["collection"],
-                [
-                    "from" => $messageData["from"],
-                    "to" => $messageData["to"],
-                    "participants" => $messageData["participants"],
-                    "message" => $messageData["message"],
-                    "timestamp" => new FirestoreTimestamp(),
-                ]);
-
+        if ($this->firestoreService->isSuccess()) {
             return new Response('', Response::HTTP_CREATED);
         } else {
             return new Response('', Response::HTTP_INTERNAL_SERVER_ERROR);
         }
+
+    }
+
+    public
+    function cleanInput($input): string
+    {
+        $input = strip_tags($input, '<br>,<img>');
+        $input = preg_replace('#<img(.*?)src="(.*?)"(.*?)>#', '<img src="$2" alt="gif">', $input);
+        return htmlentities($input);
     }
 }
