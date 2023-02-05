42c0307cceeab0ae3c5b8b67cd1ed2628bebd0f0
Lucas-allard <allard.lucas.97@gmail.com>
2023-02-05 12:14:52 +0100

    add a router logic

--- assets/react/controllers/App.jsx
+++ assets/react/controllers/App.jsx
@@ -1,40 +1,12 @@
 import React from 'react';
 import {Provider} from "react-redux";
 import {store} from '../store/store';
-import {BrowserRouter, Route, Routes} from "react-router-dom";
-import ChatPage from "../layout/ChatPage";
-import HomePage from "../layout/HomePage";
-import DashboardPage from "../layout/DashboardPage";
+import Router from "../routes/router";
 
-function App({user, users}) {
+function App({user}) {
     return (
         <Provider store={store}>
-            <BrowserRouter>
-                <Routes>
-                    <Route path='/' element={<HomePage/>}/>
-                    {user &&
-                        <>
-                            <Route
-                                path='/chat'
-                                element={
-                                    <ChatPage
-                                        user={user}
-                                        users={users}
-                                    />
-                                }
-                            />
-                            <Route
-                                path='/dashboard'
-                                element={
-                                    <DashboardPage
-                                        user={user}
-                                    />
-                                }
-                            />
-                        </>
-                    }
-                </Routes>
-            </BrowserRouter>
+            <Router user={user} />
         </Provider>
     );
 }
